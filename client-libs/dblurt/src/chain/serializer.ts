330cd9580e744bcd7fea3aea8eda25251817abe8
Julian Gonzalez <joticajulian@gmail.com>
2021-03-24 00:40:28 +0100

    fix: dblurt percent_blurt

--- client-libs/dblurt/src/chain/serializer.ts
+++ client-libs/dblurt/src/chain/serializer.ts
@@ -328,6 +328,7 @@ OperationSerializers.comment_options = OperationDataSerializer(13, [
   ["author", StringSerializer],
   ["permlink", StringSerializer],
   ["max_accepted_payout", AssetSerializer],
+  ["percent_blurt", UInt16Serializer],
   ["allow_votes", BooleanSerializer],
   ["allow_curation_rewards", BooleanSerializer],
   [
